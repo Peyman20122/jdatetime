eb3053f5cd2ce32cb5bb6222c368e0d7b60bf62a
Peyman Daei Reaei <peimandaii2012@gmail.com>
2026-06-03 10:45:29 +0330

    Update test_jdate.py

--- tests/test_jdate.py
+++ tests/test_jdate.py
@@ -1,12 +1,11 @@
 import datetime
 import pickle
 import time
-# from unittest import TestCase
-from unittest
+import unittest
+from unittest import TestCase
 
 import jdatetime
 from tests import load_pickle
-
 
 class TestJDate(TestCase):
     def test_as_locale_returns_same_date_with_specified_locale(self):
